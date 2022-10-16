7db5ff1aad5e86243893a243f3d69a298d1fed23
Sarkash Talib <sarkash.talib@gmail.com>
2022-10-17 00:23:11 +0100

    intial commit, navbar

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <>
+      <div >
+        <img src="/images/bruno-kelzer-LvySG1hvuzI-unsplash.jpg" alt="" className=""/>
+        
+      </div>
+    </>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
+  )
+}
